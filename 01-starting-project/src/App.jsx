0fa828743c85563014b7b42ec3cb8895916a91a3
Jovan Zejnula <jovanzeka97@gmail.com>
2025-06-26 19:41:20 +0200

    Split components

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,38 @@
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
+import { CORE_CONCEPTS } from "./data";
+import TabButton from "./components/Example/TabButton";
+
+function App() {
+  function handleClick(clickedButton){
+    console.log(clickedButton)
+    console.log("PERA")
+  }
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => handleClick('components')}>Components</TabButton>
+            <TabButton onClick={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onClick={() => handleClick('props')}>Props</TabButton>
+            <TabButton onClick={() => handleClick('state')}>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
